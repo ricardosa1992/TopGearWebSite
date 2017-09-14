ccf9a690d01ac071246b985c3bc0d227163332bb
Ricardo <Ricardo@DESKTOP-F63HON1>
2017-09-14 17:56:45 -0300

    Add project files.

---
+++ Trabalho20172/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Trabalho20172.MvcApplication" Language="C#" %>
---
+++ Trabalho20172/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Trabalho20172.Startup))]
+namespace Trabalho20172
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
