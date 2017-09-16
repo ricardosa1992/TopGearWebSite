c1a01b276081640aa68905303915f058fedbfd20
Cayo Paulo Donatti <donatti.dev@gmail.com>
2017-09-16 15:14:35 -0300

    Criado projeto com funções de acesso à API

---
+++ TopGear.Api/Models/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopGear.Api.Models
+{
+    public class Response<T>
+    {
+        public bool Sucesso { get; set; }
+
+        public T Dados { get; set; }
+    }
+}
---
+++ TopGear.Api/TopGearApi.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using TopGear.Api.Models;
+
+namespace TopGear.Api
+{
+    public static class TopGearApi<T>
+    {
+        private static HttpClient client = new HttpClient
+        {
+            BaseAddress = new Uri("https://topgearapi.azurewebsites.net/api/")
+        };
+
+        public static Response<T> Get(string relativePath)
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.GetAsync(relativePath).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return response.Content.ReadAsAsync<Response<T>>().Result;
+            }
+            else return new Response<T> { Sucesso = false };
+        }
+
+        public static Response<T> Post(T objeto, string relativePath)
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.PostAsJsonAsync(JsonConvert.SerializeObject(objeto), relativePath).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return response.Content.ReadAsAsync<Response<T>>().Result;
+            }
+            else return new Response<T> { Sucesso = false };
+        }
+
+        public static Response<T> Put(T objeto, int id, string relativePath)
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.PutAsJsonAsync(JsonConvert.SerializeObject(objeto), 
+                                                                    relativePath + "/" + id.ToString()).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return response.Content.ReadAsAsync<Response<T>>().Result;
+            }
+            else return new Response<T> { Sucesso = false };
+        }
+
+        public static Response<T> Delete(int id, string relativePath)
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.DeleteAsync(relativePath + "/" + id.ToString()).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return response.Content.ReadAsAsync<Response<T>>().Result;
+            }
+            else return new Response<T> { Sucesso = false };
+        }
+    }
+}
