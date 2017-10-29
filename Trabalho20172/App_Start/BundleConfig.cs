7c329ec1fa4d05a00e108d520ab5a01e9b1aadf7
RICARDO <RICARDO@DESKTOP-JHQTRMD>
2017-10-29 08:50:58 -0200

    Acertando a inclusão das malditas folhas de estilo

--- Trabalho20172/App_Start/BundleConfig.cs
+++ Trabalho20172/App_Start/BundleConfig.cs
@@ -9,14 +9,14 @@ namespace Trabalho20172
         public static void RegisterBundles(BundleCollection bundles)
         {
 
-            bundles.Add(new ScriptBundle("~/bundles/js").
-          IncludeDirectory("~/Scripts/js/", "*.js", true));
+          //  bundles.Add(new ScriptBundle("~/bundles/js").
+          //IncludeDirectory("~/Scripts/js/", "*.js", true));
 
             bundles.Add(new ScriptBundle("~/bundles/js-rental-car").
           IncludeDirectory("~/Scripts/js-rental-car/", "*.js", true));
 
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/js/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -30,9 +30,16 @@ namespace Trabalho20172
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                    "~/Scripts/bootstrap.js",
+                    "~/Scripts/respond.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/animate.css",
+                      "~/Content/datepicker.css",
+                      "~/Content/styles.css",
+                      "~/Content/rentalcar-template.css"));
 
             bundles.Add(new StyleBundle("~/Content/css/rental-car").
             IncludeDirectory("~/Content/css/rental-car-css/", "*.css", true));
